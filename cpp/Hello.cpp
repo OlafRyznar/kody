9f9f10e831525cddafd5a0ee45e25f7a2470b54a
OlafRyznar <olaf.ryznar@lo1.sandomierz.pl>
2020-09-11 09:11:04 +0200

    dodanie plików zdrodlowych cpp i pythona

---
+++ cpp/Hello.cpp
@@ -0,0 +1,13 @@
+/*
+ * Hello.cpp
+ * wyświetlenie prostego kodu w terminalu
+ */
+ 
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::cout << "hello!";
+	return 0;
+}
+
---
+++ python/pole_kola.py
@@ -0,0 +1,8 @@
+def main(args):
+    r = input("Podaj promien:")
+    pole = r * r * 3.14
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
